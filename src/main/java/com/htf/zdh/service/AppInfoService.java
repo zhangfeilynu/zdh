ef3af31f61210c2db80f627fc9219c0cbb38e362
fei.zhang <fei.zhang@chinaredstar.com>
2019-08-26 16:11:33 +0800

    数据库添加字段、FileService里面上传APP的方法合并到APPInfoService

--- src/main/java/com/htf/zdh/service/AppInfoService.java
+++ src/main/java/com/htf/zdh/service/AppInfoService.java
@@ -1,10 +1,18 @@
 package com.htf.zdh.service;
 
+import org.springframework.web.multipart.MultipartFile;
+
+import com.htf.zdh.controller.vo.Result;
 import com.htf.zdh.jdbc.po.AppInfoList;
+import com.htf.zdh.service.bo.AppInfoBo;
 import com.htf.zdh.service.bo.AppInfoListBo;
 
 public interface AppInfoService {
 
 	public AppInfoListBo selectApps(AppInfoList appInfoList, Integer pageNum, Integer pageSize);
+
+	public Result<AppInfoBo> uploadFile(AppInfoBo appInfo, MultipartFile file);
+
+	public int saveAppInfo(AppInfoList record);
 
 }
