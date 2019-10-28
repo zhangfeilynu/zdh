aa52665f40c56ef445c082ac3d5e31e808464172
fei.zhang <fei.zhang@chinaredstar.com>
2019-10-28 08:31:53 +0800

    添加double相除四舍五入保留4位小数接口

--- src/main/java/com/htf/zdh/controller/UtilController.java
+++ src/main/java/com/htf/zdh/controller/UtilController.java
@@ -10,6 +10,7 @@ import com.htf.zdh.utils.MathUtil;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 
 @Api(tags = { "小工具" })
@@ -17,13 +18,25 @@ import io.swagger.annotations.ApiOperation;
 @RequestMapping(value = "util")
 public class UtilController {
 
-	@ApiOperation(value = "四舍五入保留2位小数", notes = "四舍五入保留2位小数")
+	@ApiOperation(value = "四舍五入保留4位小数", notes = "四舍五入保留4位小数")
 	@ApiImplicitParam(name = "num", value = "小数", required = true, paramType = "query", dataType = "double")
-	@RequestMapping(value = "/formatDoubleLeafTwo", method = { RequestMethod.GET })
+	@RequestMapping(value = "/formatDoubleLeafFour", method = { RequestMethod.GET })
 	@ResponseBody
-	public String formatDoubleLeafTwo(@RequestParam double num) {
+	public String formatDoubleLeafFour(@RequestParam double num) {
 
-		return MathUtil.formatDoubleLeafTwo(num);
+		return MathUtil.formatDoubleLeafFour(num);
+
+	}
+
+	@ApiOperation(value = "2个double相除四舍五入保留4位小数", notes = "2个double相除四舍五入保留4位小数")
+	@ApiImplicitParams({
+			@ApiImplicitParam(name = "dividend", value = "被除数", required = true, paramType = "query", dataType = "double"),
+			@ApiImplicitParam(name = "divisor", value = "除数", required = true, paramType = "query", dataType = "double") })
+	@RequestMapping(value = "/formatDoubleLeafN", method = { RequestMethod.POST, RequestMethod.GET })
+	@ResponseBody
+	public String formatDoubleLeafN(@RequestParam double dividend, @RequestParam double divisor) {
+
+		return MathUtil.formatDoubleLeafN(dividend, divisor);
 
 	}
 
--- src/main/java/com/htf/zdh/utils/MathUtil.java
+++ src/main/java/com/htf/zdh/utils/MathUtil.java
@@ -2,7 +2,23 @@ package com.htf.zdh.utils;
 
 public class MathUtil {
 
-	public static String formatDoubleLeafTwo(double d) {
-		return String.format("%.2f", d);
+	public static void main(String[] args) {
+
+		// double a = 9487.65;
+		// double b = 478712.55;
+		// System.out.println(a / b);
+		System.out.print(formatDoubleLeafN(9385.74, 43513.50));
+
+	}
+
+	public static String formatDoubleLeafFour(double d) {
+		return String.format("%.4f", d);
+	}
+
+	public static String formatDoubleLeafN(double dividend, double divisor) {// dividend 被除数 divisor 除数
+		if (divisor == 0L)
+			return "除数不能为0";
+		double result = dividend / divisor;
+		return String.format("%.4f", result);
 	}
 }
